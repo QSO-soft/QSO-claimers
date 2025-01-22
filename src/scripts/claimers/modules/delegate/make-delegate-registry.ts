2b53cca44ed5fa8941441bb1b3ae8534b5c4dcee
Mokekula <dmitriy.sedykh33@gmail.com>
2025-01-23 01:05:36 +0400

    up delegate registry

--- src/scripts/claimers/modules/delegate/make-delegate-registry.ts
+++ src/scripts/claimers/modules/delegate/make-delegate-registry.ts
@@ -1,5 +1,13 @@
-import { TransactionCallbackParams, TransactionCallbackReturn, transactionWorker } from '../../../../helpers';
-import { TransformedModuleParams } from '../../../../types';
+import {
+  decimalToInt,
+  getGasOptions,
+  getTrimmedLogsAmount,
+  intToDecimal,
+  TransactionCallbackParams,
+  TransactionCallbackReturn,
+  transactionWorker,
+} from '../../../../helpers';
+import { Tokens, TransformedModuleParams } from '../../../../types';
 import { abi, CONTRACT } from './constants';
 
 export const execMakeDelegateRegistry = async (params: TransformedModuleParams) =>
@@ -10,9 +18,16 @@ export const execMakeDelegateRegistry = async (params: TransformedModuleParams) 
   });
 
 const makeDelegateRegistry = async (params: TransactionCallbackParams): TransactionCallbackReturn => {
-  const { client, logger, wallet, network } = params;
+  const { client, logger, wallet, network, gweiRange, gasLimitRange } = params;
   const { walletAddress, delegateToAddress } = wallet;
-  const { explorerLink, walletClient, publicClient } = client;
+  const {
+    explorerLink,
+    walletClient,
+    publicClient,
+    chainData: {
+      nativeCurrency: { decimals, symbol },
+    },
+  } = client;
 
   if (!delegateToAddress) {
     return {
@@ -20,7 +35,6 @@ const makeDelegateRegistry = async (params: TransactionCallbackParams): Transact
       message: 'Please provide delegateToAddress to delegate-wallets.csv',
     };
   }
-  logger.info(`Delegating from ${walletAddress} to ${delegateToAddress} in ${network}...`);
 
   const rights = '0x0000000000000000000000000000000000000000000000000000000000000000';
   const isAlreadyDelegated = await publicClient.readContract({
@@ -36,12 +50,68 @@ const makeDelegateRegistry = async (params: TransactionCallbackParams): Transact
     };
   }
 
+  const feeOptions = await getGasOptions({
+    gweiRange,
+    gasLimitRange,
+    network,
+    publicClient,
+  });
+
+  const args = [delegateToAddress, rights, true];
+  let fee = 0n;
+  if (network === 'eth') {
+    fee = intToDecimal({
+      amount: 0.005,
+      decimals,
+    });
+  }
+  if (network === 'polygon') {
+    fee = intToDecimal({
+      amount: 1,
+      decimals,
+    });
+  }
+  if (network === 'bsc') {
+    fee = intToDecimal({
+      amount: 0.01,
+      decimals,
+    });
+  }
+  if (!fee) {
+    const gasPrice = await publicClient.getGasPrice();
+    const maxPriorityFeePerGas = (await publicClient.estimateMaxPriorityFeePerGas()) || BigInt(1e9);
+    const contractGas = await publicClient.estimateContractGas({
+      address: CONTRACT,
+      abi,
+      functionName: 'delegateAll',
+      args,
+      ...feeOptions,
+    });
+
+    const feeGasPathBigInt = Number(contractGas * (gasPrice + maxPriorityFeePerGas)) * 0.2;
+    const feeGasPath = +feeGasPathBigInt.toFixed(0) / 10 ** decimals;
+    const feeInt = +feeGasPath.toFixed(5);
+
+    fee = intToDecimal({ amount: feeInt, decimals });
+  }
+
+  logger.info(
+    `Delegating from ${walletAddress} to ${delegateToAddress} with value [${getTrimmedLogsAmount(
+      decimalToInt({
+        amount: fee,
+        decimals,
+      }),
+      symbol as Tokens
+    )}]...`
+  );
+
   const txHash = await walletClient.writeContract({
     address: CONTRACT,
     abi,
     functionName: 'delegateAll',
-    args: [delegateToAddress, rights, true],
-    // value,
+    args,
+    value: fee,
+    ...feeOptions,
   });
 
   const resMsg = `Delegated registry ${walletAddress} > ${delegateToAddress} in ${network}`;
